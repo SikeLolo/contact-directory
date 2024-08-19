0b298a042c7fd346bdbb14824517a308aca67856
kyan <tbnrthunder9@gmail.com>
2024-08-19 16:50:21 -0400

    Init backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+mongoose.connect('mongodb://127.0.0.1/contactDirectory').then(() => {
+    console.log('Connected to MongoDB')
+}).catch((error) => {
+    console.error('Error connecting to the server', error)
+})
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+dotenv.config()
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}`)
+})
