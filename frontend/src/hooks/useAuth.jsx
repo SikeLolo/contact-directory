575b11556d6b118801694125f76e2c2f27e3fd9b
kyan <tbnrthunder9@gmail.com>
2024-08-23 01:39:41 -0400

    Init useAuth context

---
+++ frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from 'react'
+
+const AuthContext = createContext();
+
+const useAuth = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default useAuth
